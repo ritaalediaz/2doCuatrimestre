bfd8b0d45ae2a8964065bbadc68f1bf3789f72be
Rita <ritaalediaz@gmail.com>
2024-10-09 19:15:46 -0300

    modificacion hijos  herencia

--- regAutomotorHerencia/camion.ts
+++ regAutomotorHerencia/camion.ts
@@ -1,10 +1,22 @@
-import {Vehiculo} from "./vehiculos";
+
+
+import {Vehiculo} from "./vehiculos"
+
+const numEjes =2; // inicio de 2 ejes
+const capacidadCarga=800; // inicio de carga desde 800 kg
 
 export class Camion extends Vehiculo{
+    
+    private ejes: number
+    private carga: number 
 
-
-    constructor(marca:string, modelo: string, anio:number, color:string){
-        super(marca, modelo, anio, color)
-        
-    }
+    constructor(marca:string, modelo: string, anio:number, color:string, ejes:number, carga:number){
+    super(marca, modelo, anio, color)
+    this.ejes= numEjes;
+    this.carga= capacidadCarga;
+      
 }
+
+}
+    
+    
--- regAutomotorHerencia/main.ts
+++ regAutomotorHerencia/main.ts
@@ -4,107 +4,87 @@ import {Auto} from "./auto"
 import { Moto } from './moto';
 import { Camion } from "./camion";
 
+
 //arreglo de vehiculos
 
+
 let registro = new RegistroAutomotor();
-
-let ford: Auto = new Auto("Ford", "Ka", 2010, "Rojo",2);
-let Gilera: Moto = new Moto("Gilera", "Smash Tunnig", 2022, "Azul");
-let siena: Auto= new Auto("Fiat", "Siena", 2019, "Gris", 5);
-
-let scania: Camion = new Camion
-
-("Scania", "R 450",2011, "Blanco");
+let Ford: Auto = new Auto ("Ford", "Ka", 2010, "Rojo", 3);
+let Gilera: Moto = new Moto ("Gilera", "Smash Tunnig", 2022, "Azul",125);
+let Siena: Auto= new Auto ("Fiat", "Siena", 2019, "Gris", 5);
+let Scania: Camion = new Camion ("Scania", "R 450",2011, "Blanco", 2, 800);
 
 
 // Agregar vehículos al registro
-
-registro.agregarVehiculo(ford);
+registro.agregarVehiculo(Ford);
 registro.agregarVehiculo(Gilera);
-registro.agregarVehiculo(siena);
-registro.agregarVehiculo(scania);
+registro.agregarVehiculo(Siena);
+registro.agregarVehiculo(Scania);
 
 
 // Listar vehículos
-
 console.log("----------Lista de vehículos en el registro:");
 console.log(registro.getListaVehiculo()); // Llama al método para obtener la lista de vehículos.
 
 
 // Eliminar vehiculo.
-
-registro.eliminarVehiculo(ford);
-
+registro.eliminarVehiculo(Ford);
 console.log("ELIMINAR VEHICULO")
-console.log("-------El vehículo eliminado es: " , ford);
+console.log("-------El vehículo eliminado es: " , Ford);
 
 
 //actualizamos el registro
-
 console.log("------------registro actualizado------------------")
 console.log(registro.getListaVehiculo());
 
 
-
-
 //agregamos una moto por el usuario
-
 console.log("----------AÑADA UNA MOTO AL REGISTRO:---------\n");
-
 let marca: string = rls.question("Ingrese la marca del vehículo: ");
 let modelo: string = rls.question("Ingrese el modelo del vehículo: ");
 let anio: number = rls.question("Ingrese el año de fabricación del vehículo: ");
 let color: string = rls.question("Ingrese el color del vehículo:\n");
-
-
-let nuevaMoto: Moto = new Moto(marca,modelo,anio,color);
+let nuevaMoto: Moto = new Moto(marca,modelo,anio,color,125);
 
 
 // y la agregamos al registro
-
 registro.agregarVehiculo(nuevaMoto);
-
 console.log("------la Moto se añadio con exito:-----------\n");
 
 
 // actualizamos lista
-
 console.log("---------------------registro actualizado-----------------")
 console.log(registro.getListaVehiculo());
 
 
-
 //modificamos un vehiculo
-
 console.log("MODIFICAR COLOR:")
 console.log(Gilera);
+
+
 // Modifico el color de una moto desde su metodo de clase.
 Gilera.setColor("Rojo")
 console.log(Gilera)
 console.log ("---------------!color modificado con exito¡\n")
-
-
 // de esta manera, podria modificar el color de cualquier vehiculo o remplazar todo
+
+
 
 //reemplazo camion
 console.log(  "REEMPLAZAR CAMION:\n")
-console.log(scania);
+console.log(Scania);
 console.log(" remplazar por el camion\n:")
 
-// desde su metodo de clase.
 
-scania.setMarca("iveco")
-scania.setModelo("rouser")
-scania.setAnio(1998)
-scania.setColor("negro")
-console.log(scania)
+// desde su metodo de clase.
+Scania.setMarca("iveco")
+Scania.setModelo("rouser")
+Scania.setAnio(1998)
+Scania.setColor("negro")
+console.log(Scania)
 console.log ("---!!Camion remplazado con exito¡¡---\n")
 
 
-
 //actualizo de nuevo
-
 console.log("------------------registro nuevamente actualizado:------------------------")
 console.log(registro);
-
-
