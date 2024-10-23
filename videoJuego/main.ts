494c8b9edc8fce9ef89c7eed386e4023132ee54b
Rita <ritaalediaz@gmail.com>
2024-10-23 19:41:10 -0300

    class mago legendario

--- videoJuego/main.ts
+++ videoJuego/main.ts
@@ -4,6 +4,7 @@ import {Caballero} from "./caballero";
 import {Verdugo} from "./verdugo";
 import {Ladydi} from "./ladydi";
 import {Juego} from "./Juego";
+import{MagoMejorado} from "./magoMejorado";
 
 const juego = new Juego();
 
@@ -11,11 +12,13 @@ let mago:Mago = new Mago("Gandalf", 1, "Hechizos",100,5,30,"convertir en sapo");
 let caballero:Caballero = new Caballero("Conan", 1,"Fuerza Física",100,10,20,"Escudo Potente");
 let verdugo:Verdugo = new Verdugo("Legolas", 1, "Tortura",100,5,40,"hacha de verdugo" );
 let ladydi:Ladydi = new Ladydi("Diana", 1, "Voz Destructiva" ,100,7,25,"baile de lucha");
+let magoMejorado:MagoMejorado = new MagoMejorado("Gandalf", 1, "Hechizos con los cuatro elementos",100,4,50,"Magia con los cuatro elementos:Agua-Fuego-Aire-Tierra");
 
 juego.agregarPersonaje(caballero);
 juego.agregarPersonaje(mago);
 juego.agregarPersonaje(verdugo);
 juego.agregarPersonaje(ladydi);
+juego.agregarPersonaje(magoMejorado);
 
 juego.iniciarJuego();
 console.log("El Caballero está preparado para atacar\n")
@@ -27,7 +30,7 @@ caballero.setNivel(2)
 console.log(Caballero)
 console.log ("!Nivel y arma cambiado con exito¡\n")
 
-mago.evolucionar();
+magoMejorado.evolucionar();
 console.log("El mago esta preparado para evolucionar a Mago Legendario")
 console.log("El Mago evolucionó")
 console.log(Mago)
